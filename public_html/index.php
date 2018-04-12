6e710fd25849d7b305a2b73c5f5458c5fdf8bd8f
Nate Nolting <me@natenolting.com>
2018-04-12 14:53:48 -0500

    proposed changes

--- public_html/index.php
+++ public_html/index.php
@@ -1,19 +1,32 @@
+<?php 
+    $content = 'home';
+    if (isset($_GET['p']) && file_exists('pages/'. $_GET['p'] . '.php')) {
+       $content = $_GET['p'];
+    } elseif (isset($_GET['p']) && !file_exists('pages/'. $_GET['p'] . '.php')) {
+        $content = '404';
+    }
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>M & N Paints</title>
+<!--<title>M & N Paints</title>-->
+  <?php include __DIR__ . '/head.html';  ?> 
 </head>
 <link rel="stylesheet" type="text/css" href="style.css"/>
 <body>
-  <h1>M & N Paints</h1>
+  <?php include __DIR__ . '/masthead.html';  ?> 
+  <?php include __DIR__ . '/navigation.html';  ?> 
+<!--   <h1>M & N Paints</h1>
     <p>Welcome to M & N Paints. We have the best granite paints and the best prices in the country.</p>
     <img src="http://jacksonmonumentworks.com/granite-colors/"
- alt="Color Options"/>
+ alt="Color Options"/> -->
+  <?php include __DIR__ . $content . '.php'; ?>
 
 
 
 
-
+  <?php include __DIR__ . '/footer.php';  ?> 
 
 </body>
 </html>
